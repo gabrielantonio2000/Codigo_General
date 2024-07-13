5ff9cc44b9867c5c9cd1ee143216840a4f247311
gabrielantonio2000 <gabrielantonio21oc@gmail.com>
2024-07-12 19:35:41 -0600

    codigo 04

--- Devtalles/curso-React/02-bases-javascript/src/index.js
+++ Devtalles/curso-React/02-bases-javascript/src/index.js
@@ -1,17 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+const arreglo = [1, 2, 3, 4]
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+//arreglo.push(2)
+//arreglo.push(3)
+//arreglo.push(4)
+//arreglo.push(1)
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+let arreglo2 = [...arreglo, 5];
+
+const arreglo3 = arreglo2.map(function (numero) {
+  return numero * 2;
+});
+
+
+
+console.log(arreglo);
+console.log(arreglo2);
+console.log(arreglo3);
