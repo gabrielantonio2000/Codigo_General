f8832873b1b2596ed4f998b4c382493ae8ba785d
gabrielantonio2000 <gabrielantonio21oc@gmail.com>
2024-12-15 20:17:15 -0600

    cambios

--- Devtalles/Curso-JavaScript/01-FUNDAMENTOS/app.js
+++ Devtalles/Curso-JavaScript/01-FUNDAMENTOS/app.js
@@ -7,8 +7,8 @@ let c=30;
 let d=40;
 let x = a+b;
 
-console.log('x', x);
-console.log('a', a)
-console.log('b', b)
-console.log('c', c)
-console.log('d', d)
+console.log( {x} );
+console.log( {a})
+console.log( {b})
+console.log( {c})
+console.log( {d})
