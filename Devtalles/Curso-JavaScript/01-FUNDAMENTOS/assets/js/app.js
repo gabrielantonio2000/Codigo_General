f0fd82d45cc79ca5628a7865826d5c8511f5c921
gabrielantonio2000 <gabrielantonio21oc@gmail.com>
2024-12-15 21:55:19 -0600

    cambios

--- Devtalles/Curso-JavaScript/01-FUNDAMENTOS/assets/js/app.js
+++ Devtalles/Curso-JavaScript/01-FUNDAMENTOS/assets/js/app.js
@@ -1,24 +1,28 @@
 // console.log('HOla mundo');
 
 
-alert('hola desde app.js');
 
+
+alert('hola desde app.js');
 let a= 10;
 let b=20;
 let c='hola ';
 let d='Lampard';
 let x = a+b;
 
-console.log( '%c MIs variables', 'color:with; font-weight: bold' );
-console.log( {a})
-console.log( {b})
-console.log( {c})
-console.log( {d})
+//console.log( '%c MIs variables', 'color:with; font-weight: bold' );
+//console.log( {a})
+//console.log( {b})
+//console.log( {c})
+//console.log( {d})
 
 
-console.table({a,b,c,d})
+//console.table({a,b,c,d})
 
 
 c='HOla de nuevo';
 
 const SALUDO= c+d;
+
+
+var Minombre= 'Gabriel';
