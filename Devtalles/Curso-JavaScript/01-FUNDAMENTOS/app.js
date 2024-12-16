43f3005d4c94246be90073766f9fbd143dcb654c
gabrielantonio2000 <gabrielantonio21oc@gmail.com>
2024-12-15 20:18:50 -0600

    cambios

--- Devtalles/Curso-JavaScript/01-FUNDAMENTOS/app.js
+++ Devtalles/Curso-JavaScript/01-FUNDAMENTOS/app.js
@@ -7,7 +7,7 @@ let c=30;
 let d=40;
 let x = a+b;
 
-console.log( {x} );
+console.log( '%c MIs variables', 'color:with; font-weight: bold' );
 console.log( {a})
 console.log( {b})
 console.log( {c})
