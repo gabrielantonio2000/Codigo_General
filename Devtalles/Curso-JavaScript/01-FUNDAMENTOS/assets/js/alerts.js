f39543ca7b80cfb4a29ebe003b5eb02e951e53c1
gabrielantonio2000 <gabrielantonio21oc@gmail.com>
2024-12-16 21:33:23 -0600

    cambios

---
+++ Devtalles/Curso-JavaScript/01-FUNDAMENTOS/assets/js/alerts.js
@@ -0,0 +1,9 @@
+alert('hola mundo')
+
+let nombre = prompt('dame un dato', );
+
+console.log('*****' + nombre + '*****');
+
+
+ const selccion = confirm('Estás seguro de borrar esto');
+ console.log(selccion);
