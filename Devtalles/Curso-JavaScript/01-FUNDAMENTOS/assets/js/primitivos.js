4cd5a59e7a01afe5c8d883784b81587a67fb0ac4
gabrielantonio2000 <gabrielantonio21oc@gmail.com>
2024-12-18 19:00:18 -0600

    cambios

---
+++ Devtalles/Curso-JavaScript/01-FUNDAMENTOS/assets/js/primitivos.js
@@ -0,0 +1,40 @@
+let nombre = 'Peter parker';
+console.log(nombre);
+
+
+nombre = 'Ben Parker';
+console.log(nombre);
+
+nombre = "Tia may" ;
+nombre = `Tia may`;
+
+console.log(typeof nombre);
+
+nombre= 123;
+console.log(typeof nombre);
+
+let esMarvel = true;
+
+console.log(esMarvel);
+
+
+let edad= 33;
+console.log(typeof edad);
+
+ edad= 33.11;
+console.log(typeof edad);
+
+
+let superPoder;
+console.log(superPoder);
+
+let soyNull= null;
+console.log(soyNull);
+
+
+let symbol1= Symbol('a');
+let symbol2= Symbol('a');
+
+console.log(symbol1);
+
+console.log(symbol1 === symbol2);
