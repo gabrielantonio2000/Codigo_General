2a38a59364b4ad80523512668249af5def79b5f0
gabrielantonio2000 <gabrielantonio21oc@gmail.com>
2024-12-19 10:28:11 -0600

    cambios

---
+++ Devtalles/Curso-JavaScript/01-FUNDAMENTOS/assets/js/arreglos.js
@@ -0,0 +1,26 @@
+//forma no muy común de crear arreglos
+//const arr = new Array(10);
+//console.log(arr);
+
+
+//forma más común de crear arreglos
+//const arrr= [];
+//console.log(arrr);
+
+
+
+let videoJuegos = ['mario 3', 'megaman', 'Skyrim' ];
+console.log(  {videoJuegos} );
+
+console.log(videoJuegos [0]);
+
+let arregloCosas= [
+    true,
+    123,
+    'gabriel',
+    1+2
+];
+
+console.log(arregloCosas);
+
+
--- Devtalles/Curso-JavaScript/01-FUNDAMENTOS/assets/js/palabras-reservadas.js
+++ Devtalles/Curso-JavaScript/01-FUNDAMENTOS/assets/js/palabras-reservadas.js
@@ -1,1 +1,11 @@
 let constante = 123;
+//const
+// if
+// try
+// while
+// for()
+// enum
+//let enum= 123;
+//let await = 'a';
+
+
