0e1093fe5febef4c24b21acfcfa3165f4895c0dd
gabrielantonio2000 <gabrielantonio21oc@gmail.com>
2024-12-20 21:42:06 -0600

    cambios

---
+++ Devtalles/Curso-JavaScript/01-FUNDAMENTOS/assets/js/objeto-literal.js
@@ -0,0 +1,27 @@
+let personaje= {
+    nombre: 'Tony Stark',
+    codeName: 'IronMan',
+    vivo: false,
+    edad: 40,
+    coords: {
+        lat: 34.034,
+        lng: -118.70,
+    },
+
+    trajes: ['mark I', 'Mark V', 'Hulkbuster'],
+    direccion: {
+        zip: '10880, 90265',
+        ubicacion: 'Malibu, Calofornia'
+    }
+};
+
+
+console.log(personaje);
+console.log('Nombre', personaje.nombre);
+
+console.log('Nombre', personaje['nombre']);
+console.log('Edad', personaje.edad);
+
+console.log('Coors', personaje.coords.lat);
+
+console.log('No.trajes', personaje.trajes.length);
