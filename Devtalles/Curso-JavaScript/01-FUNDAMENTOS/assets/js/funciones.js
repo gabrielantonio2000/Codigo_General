1ddb394becb23eefda628d925aca31b40008970a
gabrielantonio2000 <gabrielantonio21oc@gmail.com>
2024-12-27 21:29:00 -0600

    cambios

--- Devtalles/Curso-JavaScript/01-FUNDAMENTOS/assets/js/funciones.js
+++ Devtalles/Curso-JavaScript/01-FUNDAMENTOS/assets/js/funciones.js
@@ -1,6 +1,10 @@
 function saludar(nombre){
     console.log(arguments);
     console.log('Hola ' + nombre);
+    return 10;
+    //en una función después de su return que es el punto limite, si agregamos al abajo
+    //del return ya no lo va a tomar
+    console.log('soy un codigo que esta después del return');
 }
 
 
@@ -8,22 +12,41 @@ const saludar2= function(nombre){
     console.log('hola ' + nombre);
 }
 
-
-
-saludar2('Antonio');
-saludar('Gabriel', 40, true, 'Guatemala');
-
+//const retornoDeSaludar= saludar('Gabriel', 40, true, 'Guatemala');
+//console.log({retornoDeSaludar});
 
 //funciones de flecha acá abajo
-
-
 const saludarFlecha= () =>{
     console.log('Hola Flecha');
 }
-
 const saludarFlecha2= (nombre) =>{
     console.log('Hola ' + nombre);
 }
 
-saludarFlecha();
-saludarFlecha2('Jake');
+//saludarFlecha();
+//saludarFlecha2('Jake');
+
+
+function sumar(a,b){
+    return a+b;
+}
+
+console.log( sumar(1,2));
+
+
+
+//esta es una función de flecha
+const sumar2 = (a,b) =>{
+    return a+b;
+}
+
+console.log( sumar2(2,4));
+
+//la función de flecha se puede resumir más de esta forma cuando solo hay una linea
+
+const sumar3 = (a,b) => a+b;
+
+console.log( sumar3(10,10));
+
+
+
