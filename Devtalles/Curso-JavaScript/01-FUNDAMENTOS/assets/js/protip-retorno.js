586992b321cff0e7dc07dc1f791fae60bc789d5f
gabrielantonio2000 <gabrielantonio21oc@gmail.com>
2024-12-28 23:03:14 -0600

    cambios

---
+++ Devtalles/Curso-JavaScript/01-FUNDAMENTOS/assets/js/protip-retorno.js
@@ -0,0 +1,22 @@
+// function crearPersona( nombre, apellido){
+// return {
+//     nombre: nombre,
+//     apellido: apellido
+// }
+// }
+
+
+// const persona = crearPersona( 'Gabriel', 'Antonio');
+
+// console.log (persona);
+
+//crear esto como una función de flecha
+
+const  crearPersona = ( nombre, apellido) =>{
+    return {nombre, apellido}
+}
+
+const persona = crearPersona ( 'gabriel ', 'antonio');
+console.log( persona);
+
+
