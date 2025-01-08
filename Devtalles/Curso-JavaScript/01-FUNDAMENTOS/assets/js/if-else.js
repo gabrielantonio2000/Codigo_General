2ec7be5823be1ff3ca818859c4b19c63f2364365
gabrielantonio2000 <gabrielantonio21oc@gmail.com>
2025-01-08 17:27:33 -0600

    cambios

--- Devtalles/Curso-JavaScript/01-FUNDAMENTOS/assets/js/if-else.js
+++ Devtalles/Curso-JavaScript/01-FUNDAMENTOS/assets/js/if-else.js
@@ -1,26 +1,37 @@
-let a= 2;
+// let a= 2;
 
-if(a>10){
-    console.log( 'A es mayor o igual a 10');
+// if(a>10){
+//     console.log( 'A es mayor o igual a 10');
 
-} else{
-    console.log( 'A es menor a 10');
+// } else{
+//     console.log( 'A es menor a 10');
+// }
+
+// console.log ('Fin de programa')
+// const hoy= new Date();
+// console.log( hoy);
+
+// let dia= hoy.getDate();
+// console.log({dia});
+
+// if (dia === 0){
+//     console.log('Domingo');
+// } else{
+//     console.log('No es domingo'); 
+// }
+
+dia=0;
+const diasLetras={
+
+0:  'domingo', 
+1:  'lunes',
+2:  'martes', 
+3:  'miercoles', 
+4:  'jueves',
+5:  'viernes', 
+6:  'sabado'
 }
 
-console.log ('Fin de programa')
-
-
-
-const hoy= new Date();
-
-console.log( hoy);
-
-
-let dia= hoy.getDate();
-console.log({dia});
-
-if (dia === 0){
-    console.log('Domingo');
-} else{
-    console.log('No es domingo'); 
-}
+const diasLetras2=['domingo','lunes','martes', 'miercoles', 'jueves','viernes', 'sabado'];
+    
+console.log( diasLetras2[dia] || 'Día no definido');
