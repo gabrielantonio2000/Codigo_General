2caaac52a4fac1b18e2b2b646335498a5da8d581
gabrielantonio2000 <gabrielantonio21oc@gmail.com>
2025-01-08 21:22:56 -0600

    cambios

--- Devtalles/Curso-JavaScript/01-FUNDAMENTOS/assets/js/operador-ternario.js
+++ Devtalles/Curso-JavaScript/01-FUNDAMENTOS/assets/js/operador-ternario.js
@@ -2,7 +2,7 @@
 // pero los dias de la semana abrimos a las 9
 // Entra a  un sitio webkitURL, para consultar si está abierto hoy 
 
-const dia= 1;   //0: domingo... 1:lunes....
+const dia= 6;   //0: domingo... 1:lunes....
 
 const horaActual=11;
 
@@ -12,26 +12,30 @@ let mensaje; //ESta abierto, esta cerrado, hoy abrimos a las x
 
 //evaluamos si es sabado o domingo
 
-if (dia===0 || dia===6){
-    console.log('Fin de semana');
-    horaApertura= 9;
-} else{
-    console.log('Día de semana');
-    horaApertura= 11;
-}    
+// if (dia===0 || dia===6){
 
 
-if(horaActual>= horaApertura){
-    mensaje= 'Está abierto'
-}else{ 
-    mensaje= `Está cerrado, hoy abrimos a las ${horaApertura}`  ;
+//     if ([0,6].includes(dia) ) {
+//     console.log('Fin de semana');
+//     horaApertura= 9;
+// } else{
+//     console.log('Día de semana');
+//     horaApertura= 11;
+// }  
 
-}
 
+horaApertura= ([0,6].includes(dia)) ? 9 : 11;
 
 
 
+// if(horaActual>= horaApertura){
+//     mensaje= 'Está abierto'
+// }else{ 
+//     mensaje= `Está cerrado, hoy abrimos a las ${horaApertura}`  ;
 
+// }
+
+mensaje= (horaActual>= horaApertura) ? 'Esta abierto' : `Está cerrado, hoy trabajamos a las ${horaApertura};`
 
 
 console.log({horaApertura,mensaje})
